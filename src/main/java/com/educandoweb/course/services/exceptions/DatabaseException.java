7d4e2438353c6e799f28cc25c92b7056fff0f3b7
gstv <gstv.mra@gmail.com>
2023-01-26 13:30:21 -0300

    Exception handling - Delete

---
+++ src/main/java/com/educandoweb/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.educandoweb.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
